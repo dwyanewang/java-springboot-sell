65ab9cb286472afb4a3d0cf8953982ddb83b8975
linxinzhe <linxinzhechn@qq.com>
2018-03-19 17:00:50 +0800

    feat(buyer order): dao

---
+++ src/main/java/top/linxz/sell/dataobject/OrderMaster.java
@@ -0,0 +1,34 @@
+package top.linxz.sell.dataobject;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+import top.linxz.sell.enums.OrderStatusEnum;
+import top.linxz.sell.enums.PayStatusEnum;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@DynamicUpdate
+public class OrderMaster {
+
+    @Id
+    private String orderId;
+    private String buyerName;
+    private String buyerPhone;
+    private String buyerAddress;
+    private String buyerOpenid;
+
+    //总金额
+    private BigDecimal orderAmount;
+
+    private Integer orderStatus = OrderStatusEnum.NEW.getCode();
+
+    private Integer payStatus = PayStatusEnum.WAIT.getCode();
+
+    private Date createTime;
+    private Date updateTime;
+}
