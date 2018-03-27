3bca4f73b1184de238bf4d9d4291cbf5f23642c1
linxinzhe <linxinzhechn@qq.com>
2018-03-27 11:40:27 +0800

    feat(wechat): wechat oauth2 test

---
+++ src/main/java/top/linxz/sell/controller/WechatController.java
@@ -0,0 +1,50 @@
+package top.linxz.sell.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import top.linxz.sell.enums.ResultEnum;
+import top.linxz.sell.exception.SellException;
+
+import java.net.URLEncoder;
+
+@Controller
+@RequestMapping("/wechat")
+@Slf4j
+public class WechatController {
+    @Autowired
+    private WxMpService wxMpService;
+
+    @GetMapping("/authorize")
+    public String authorize(@RequestParam("returnUrl") String returnUrl) {
+        //1.配置
+        //2.调用方法
+        String url = "http://mpsell.natapp1.cc/sell/wechat/userInfo";
+        String redirectUrl = wxMpService.oauth2buildAuthorizationUrl(url, WxConsts.OAUTH2_SCOPE_BASE, URLEncoder.encode(returnUrl));
+        log.info("【微信网页授权】获取code，result={}", redirectUrl);
+
+        return "redirect:" + redirectUrl;
+    }
+
+    @GetMapping("/userInfo")
+    public String userInfo(@RequestParam("code") String code,
+                           @RequestParam("state") String returnUrl) {
+        WxMpOAuth2AccessToken wxMpOAuth2AccessToken = new WxMpOAuth2AccessToken();
+        try {
+            wxMpOAuth2AccessToken = wxMpService.oauth2getAccessToken(code);
+        } catch (WxErrorException e) {
+            log.error("【微信网页授权】{}", e);
+            throw new SellException(ResultEnum.WECHAT_MP_ERROR.getCode(), e.getError().getErrorMsg());
+        }
+        String openId = wxMpOAuth2AccessToken.getOpenId();
+
+        return "redirect:" + returnUrl + "?openid=" + openId;
+    }
+}
--- src/main/java/top/linxz/sell/enums/ResultEnum.java
+++ src/main/java/top/linxz/sell/enums/ResultEnum.java
@@ -15,7 +15,8 @@ public enum ResultEnum {
     ORDER_DETAIL_EMPTY(16, "订单详情为空"),
     ORDER_PAY_STATUS_EMPTY(17, "订单支付状态不正确"),
     CART_EMPTY(18, "购物车不能为空"),
-    ORDER_OWNER_ERROR(19, "该订单不术语当前用户"),;
+    ORDER_OWNER_ERROR(19, "该订单不术语当前用户"),
+    WECHAT_MP_ERROR(20, "微信公众账号方面错误"),;
 
     private Integer code;
     private String message;
